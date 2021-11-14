c62a82b311e719f007b3d17da83a32ffc73e6122
mina <minaabaraghi@gmail.com>
2021-11-14 12:32:06 +0330

    add base project me inclode html css js files

---
+++ jsFile/login.js
@@ -0,0 +1,12 @@
+function funcCreate(){
+    // var firstName=document.getElementById('firstName').value;
+    // var lastName=document.getElementById('lastName').value;
+    // alert(firstName + '' + lastName)
+    // if (firstName!='' && lastName!=''){
+    // alert("لطفا بعدا امتحان کنید");
+    // }
+    // else{
+    //     alert('لطفا مقادیر را وارد کنید')
+    // }
+    alert("لطفا بعدا امتحان کنید");
+}
